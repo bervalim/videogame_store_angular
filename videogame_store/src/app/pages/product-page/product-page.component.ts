51f78e7f583ddcf451750ebc8e7a2db52d861b54
bervalim <bernardogvalim@gmail.com>
2024-04-29 17:32:55 -0300

    feat/product page logic

--- videogame_store/src/app/pages/product-page/product-page.component.ts
+++ videogame_store/src/app/pages/product-page/product-page.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ProductRequest } from '../../api/product.request';
+import { IProduct } from '../../../interfaces/product.interface';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
   imports: [],
+  providers: [ProductRequest],
   templateUrl: './product-page.component.html',
-  styleUrl: './product-page.component.scss'
+  styleUrl: './product-page.component.scss',
 })
 export class ProductPageComponent {
+  readonly productListSignal = signal<IProduct[]>([]);
+  constructor(
+    private productRequest: ProductRequest,
+    private route: ActivatedRoute
+  ) {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      if (id) {
+        this.productRequest.getTechProductsListRequest().subscribe((data) => {
+          const filteredData = data.filter((product) => product.id !== id);
+          this.productListSignal.set(filteredData);
+        });
+      }
+    });
+  }
 
+  get productList() {
+    return this.productListSignal();
+  }
 }
