e3fc748de52a1dbffd630abea3f2327c1381ed44
bervalim <bernardogvalim@gmail.com>
2024-04-29 17:54:15 -0300

    feat/product details logic

--- videogame_store/src/app/components/product-details/product-details.component.ts
+++ videogame_store/src/app/components/product-details/product-details.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ProductRequest } from '../../api/product.request';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from '../../../interfaces/product.interface';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
   imports: [],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.scss'
+  styleUrl: './product-details.component.scss',
 })
 export class ProductDetailsComponent {
+  readonly productSignal = signal<IProduct | null>(null);
+  constructor(
+    private productRequest: ProductRequest,
+    private route: ActivatedRoute
+  ) {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      if (id) {
+        this.productRequest.getTechProductRequest(id).subscribe((data) => {
+          this.productSignal.set(data);
+        });
+      }
+    });
+  }
 
+  get product() {
+    return this.productSignal();
+  }
 }
