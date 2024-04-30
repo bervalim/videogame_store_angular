7b9db3f3fd2ef09916588b6c6b8e3a9f2ae034af
bervalim <bernardogvalim@gmail.com>
2024-04-30 12:45:32 -0300

    feat/implementing redirectURL method on product list component

--- videogame_store/src/app/components/product-list/product-list.component.ts
+++ videogame_store/src/app/components/product-list/product-list.component.ts
@@ -1,19 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { IProduct } from '../../../interfaces/product.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
 })
 export class ProductListComponent {
-  // Este componente irá receber duas possíves lista
-  // uma terá todos os produtos
-  // A outra terá todos menos o referenciado pela URL.
   @Input() productList!: IProduct[];
 
   @Input() title!: string;
+
+  redirectURL(id: string) {
+    return `/product/${id}`;
+  }
 }
--- videogame_store/src/app/pages/product-page/product-page.component.ts
+++ videogame_store/src/app/pages/product-page/product-page.component.ts
@@ -3,11 +3,12 @@ import { ProductRequest } from '../../api/product.request';
 import { IProduct } from '../../../interfaces/product.interface';
 import { ActivatedRoute } from '@angular/router';
 import { ProductListComponent } from '../../components/product-list/product-list.component';
+import { ProductDetailsComponent } from '../../components/product-details/product-details.component';
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [ProductListComponent],
+  imports: [ProductListComponent, ProductDetailsComponent],
   providers: [ProductRequest],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
